b05862017dd9233e6be4593a0ae32f0ba1806284
dhowe <github.utensil@mailhero.io>
2017-02-12 21:20:54 +0800

    fix non-compiling class added SMP solver

--- src/test/OneQuad.java
+++ src/test/OneQuad.java
@@ -11,11 +11,14 @@ public class OneQuad extends PApplet {
 	}
 
 	public void setup() {
-		float[] f = new float[]{0.8424786f,0.10419211f,0.7860224f,0.14596535f,0.7459136f,0.078942426f,0.7828462f,0.0014371289f};
-		for (int i = 0; i < f.length; i++) {
-		//	f[i] *= width;
+		
+		float[] f = { 0.8424786f,0.10419211f,0.7860224f,0.14596535f,0.7459136f,0.078942426f,0.7828462f,0.0014371289f }, g = new float[f.length/2];
+		
+		for (int i = 0; i < f.length; i+=2) {
+			g[i/2] = dist(f[i], f[i+1], f[(i+2)%f.length], f[(i+3)%f.length]); 
 		}
-		Quad q = new Quad(this, f);
+		
+		Quad q = new Quad(this, f, g);
 		q.scale(width,height);
 		q.draw();
 	}
